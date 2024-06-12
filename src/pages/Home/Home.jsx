3b663008d395359620fcfc48f5c66d84543712c7
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-12 22:21:38 +0530

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Home.css'
+import front from '../../assets/front.jpg'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <img  className=""src={front} alt="" style={{ height: '600px' , width: '100%'}}/>
+      <div className='text-block'>
+        <input type='radio'/>One Way &nbsp;<input type='radio'/>Round Trip
+      </div>
+    </div>
   )
 }
 export default Home
