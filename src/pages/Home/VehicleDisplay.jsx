8b51e9e18ac9ee297e73c418e0f0fd822a6b778c
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-14 15:10:10 +0530

    commit

--- src/pages/Home/VehicleDisplay.jsx
+++ src/pages/Home/VehicleDisplay.jsx
@@ -7,31 +7,26 @@ import nissan from '../../assets/vehicle/nissan.png';
 import subaro from '../../assets/vehicle/subaro.png';
 import tesla from '../../assets/vehicle/tesla.png';
 import toyota from '../../assets/vehicle/toyota.png';
-
 const images = [
-  { src: benz, alt: 'Benz' },
-  { src: bmw, alt: 'BMW' },
-  { src: ford, alt: 'Ford' },
-  { src: jeep, alt: 'Jeep' },
-  { src: nissan, alt: 'Nissan' },
-  { src: subaro, alt: 'Subaru' },
-  { src: tesla, alt: 'Tesla' },
-  { src: toyota, alt: 'Toyota' }
+  { src: benz, alt: 'Benz',value:'Benz' },
+  { src: bmw, alt: 'BMW' ,value:'Bmv' },
+  { src: ford, alt: 'Ford' ,value:'Ford' },
+  { src: jeep, alt: 'Jeep' ,value:'Jeep' },
+  { src: nissan, alt: 'Nissan' ,value:'Nissan' },
+  { src: subaro, alt: 'Subaru' ,value:'Subaru' },
+  { src: tesla, alt: 'Tesla' ,value:'Tesla' },
+  { src: toyota, alt: 'Toyota' ,value:'Toyota' }
 ];
-
 const VehicleDisplay = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
   const prevSlide = () => {
     const index = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
     setCurrentIndex(index);
   };
-
   const nextSlide = () => {
     const index = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
     setCurrentIndex(index);
   };
-
   return (
     <div className='flex justify-center text-center flex-col'>
       <h1 className='m-8 font-bold text-4xl'>Endless Options</h1>
@@ -46,9 +41,10 @@ const VehicleDisplay = () => {
             >
               <img
                 src={image.src}
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover rounded-2xl border-slate-400 border-4"
                 alt={image.alt}
               />
+              <p className='font-bold'>{image.value}</p>
             </div>
           ))}
           <button type="button" className="absolute left-4 z-30 flex items-center justify-center p-2 bg-indigo-600 text-white rounded-full cursor-pointer group focus:outline-none" onClick={prevSlide}>
@@ -68,11 +64,5 @@ const VehicleDisplay = () => {
     </div>
   );
 }
-
 export default VehicleDisplay;
-
-
-
-
-
 
