8a063b938d0e0f73871cc8327baff6a0ffcf91e5
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-24 23:08:27 +0530

    1-commit

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,7 +5,7 @@ import Sidebar from './components/Sidebar/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
-import Order from './pages/Order/Order';
+import Booking from './pages/Booking/Booking';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -20,7 +20,7 @@ function App() {
         <Routes>
           <Route path='/add' element={<Add url={url}/>}/>
           <Route path='/list' element={<List url={url} />}/>
-          <Route path='/order' element={<Order url={url}/>}/>
+          <Route path='/booking' element={<Booking url={url}/>}/>
         </Routes>
       </div>
     </div>
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -6,19 +6,19 @@ import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='w-[18%] min-h-screen border border-gray-400 border-t-0 text-[max(1vw,1px)]'>
-        <div className="pt-[10] pl-20 flex flex-col">
-            <NavLink to='/add' className="sidebar-option flex items-center gap-[12px] border border-gray-400 border-r-0 p-[8px_10px] rounded-[3px_0px_0px_3px] cursor-pointer">
+    <div className='w-[18%] min-h-screen border border-gray-400 border-t-0 text-[max(1vw,1px)]  bg-blue-200'>
+        <div className="pt-[50] pl-5 flex flex-col gap-[20px]">
+            <NavLink to='/add' className="sidebar-option flex items-center gap-[12px] border border-gray-400 border-r-0 p-[8px_10px] rounded-[3px_0px_0px_3px] cursor-pointer bg-blue-100">
                 <img src={add_icon} alt="" />
                 <p className='hidden lg:block'>Add Cars</p>
             </NavLink>
-            <NavLink to='/list' className="sidebar-option flex items-center gap-[12px] border border-gray-400 border-r-0 p-[8px_10px] rounded-[3px_0px_0px_3px] cursor-pointer">
+            <NavLink to='/list' className="sidebar-option flex items-center gap-[12px] border border-gray-400 border-r-0 p-[8px_10px] rounded-[3px_0px_0px_3px] cursor-pointer bg-blue-100">
                 <img src={order_icon} alt="" />
                 <p className='hidden lg:block'>List Cars</p>
             </NavLink>
-            <NavLink to='/orders' className="sidebar-option flex items-center gap-[12px] border border-gray-400 border-r-0 p-[8px_10px] rounded-[3px_0px_0px_3px] cursor-pointer">
+            <NavLink to='/booking' className="sidebar-option flex items-center gap-[12px] border border-gray-400 border-r-0 p-[8px_10px] rounded-[3px_0px_0px_3px] cursor-pointer bg-blue-100">
                 <img src={order_icon} alt="" />
-                <p className='hidden lg:block'>Booking</p>
+                <p className='hidden lg:block'>Bookings</p>
             </NavLink>
          </div>
     </div>
