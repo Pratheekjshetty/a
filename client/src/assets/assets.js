dbb6ee80ed7b5c5f304737e27e3086f4b8aeb4b6
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-25 23:02:36 +0530

    3-commit

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -15,6 +15,15 @@ import car_5 from './cars/car_5.jpg'
 import car_6 from './cars/car_6.jpg'
 import car_7 from './cars/car_7.jpg'
 import car_8 from './cars/car_8.jpg'
+import car_9 from './cars/car_9.jpg'
+import car_10 from './cars/car_10.jpg'
+import car_11 from './cars/car_11.jpg'
+import car_12 from './cars/car_12.jpg'
+import car_17 from './cars/car_17.jpg'
+import car_18 from './cars/car_18.jpg'
+import car_19 from './cars/car_19.jpg'
+import car_20 from './cars/car_20.jpg'
+
 
 export const assets = {
     benz,bmw,ford,jeep,nissan,subaro,tesla,toyota,
@@ -121,68 +130,68 @@ export const vehicle_list = [
         category: "BMW"
     },{
         _id: "9",
-        name: "Mercedes-Benz",
-        image: car_1,
+        name: "Ford",
+        image: car_9,
         price: 200,
         location:"Puttur",
         description: "A luxury automotive brand known for its high-end vehicles, innovative technology, and exceptional performance.",
-        category: "Benz"
+        category: "Ford"
     },{
         _id: "10",
-        name: "Mercedes-Benz",
-        image: car_2,
+        name: "Ford",
+        image: car_10,
         price: 200,
         location:"Bantwal",
         description: "A luxury automotive brand known for its high-end vehicles, innovative technology, and exceptional performance.",
-        category: "Benz"
+        category: "Ford"
     },{
         _id: "11",
-        name: "Mercedes-Benz",
-        image: car_3,
+        name: "Ford",
+        image: car_11,
         price: 200,
         location:"Manglore",
         description: "A luxury automotive brand known for its high-end vehicles, innovative technology, and exceptional performance.",
-        category: "Benz"
+        category: "Ford"
     },{
         _id: "12",
-        name: "Mercedes-Benz",
-        image: car_4,
+        name: "Ford",
+        image: car_12,
         price: 200,
         location:"Manglore",
         description: "A luxury automotive brand known for its high-end vehicles, innovative technology, and exceptional performance.",
-        category: "Benz"
+        category: "Ford"
     },{
-        _id: "13",
-        name: "BMW",
-        image: car_5,
-        price: 200,
-        location:"Manglore",
-        description: "A leading manufacturer of luxury vehicles and motorcycles, celebrated for its sporty driving dynamics and cutting-edge design.",
-        category: "BMW"
-    },{
-        _id: "14",
-        name: "BMW",
-        image: car_6,
+        _id: "17",
+        name: "Nissan",
+        image: car_17,
         price: 200,
         location:"Puttur",
         description: "A leading manufacturer of luxury vehicles and motorcycles, celebrated for its sporty driving dynamics and cutting-edge design.",
-        category: "BMW"
+        category: "Nissan"
     },{
-        _id: "15",
-        name: "BMW",
-        image: car_7,
-        price: 200,
-        location:"Bantwal",
-        description: "A leading manufacturer of luxury vehicles and motorcycles, celebrated for its sporty driving dynamics and cutting-edge design.",
-        category: "BMW"
-    },{
-        _id: "16",
-        name: "BMW",
-        image: car_8,
+        _id: "18",
+        name: "Nissan",
+        image: car_18,
         price: 200,
         location:"Manglore",
         description: "A leading manufacturer of luxury vehicles and motorcycles, celebrated for its sporty driving dynamics and cutting-edge design.",
-        category: "BMW"
+        category: "Nissan"
+    },{
+        _id: "19",
+        name: "Nissan",
+        image: car_19,
+        price: 200,
+        location:"Bantwal",
+        description: "A leading manufacturer of luxury vehicles and motorcycles, celebrated for its sporty driving dynamics and cutting-edge design.",
+        category: "Nissan"
+    },{
+        _id: "20",
+        name: "Nissan",
+        image: car_20,
+        price: 200,
+        location:"Manglore",
+        description: "A leading manufacturer of luxury vehicles and motorcycles, celebrated for its sporty driving dynamics and cutting-edge design.",
+        category: "Nissan"
     },
 
 ]
