164d341e0e4f539763db318ec6cdc03329aa10cd
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-27 21:52:55 +0530

    2-commit

--- client/src/component/Hosts/Hosts.jsx
+++ client/src/component/Hosts/Hosts.jsx
@@ -4,6 +4,7 @@ const Hosts = () => {
   return (
     <div className='flex justify-center text-center flex-col'>
       <h1 className='m-8 font-bold text-4xl'>Meet Our Hosts</h1>
+      <p>These are some of our hosts</p>
       <p></p>
     </div>
   )
