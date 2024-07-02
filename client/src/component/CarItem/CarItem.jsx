17ae22288b93a1107ae49c74d6c85df94a06b4cf
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-07-02 17:25:34 +0530

    new-caritem updated

--- client/src/component/CarItem/CarItem.jsx
+++ client/src/component/CarItem/CarItem.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext';
 const CarItem = ({id,name,price,location,description,image}) => {
+  const {url}=useContext(StoreContext);
   return (
     <div className="w-full mx-auto rounded shadow-lg animate-fadeIn duration-2000" id="car_item">
-        <img className='w-full rounded-md'src={image} alt=""/>
+        <img className='w-full rounded-md'src={url+"/images/"+image} alt=""/>
         <div className='p-2.5'>
             <div className='flex justify-between items-center mb-2.5'>
                 <p className='text-md font-bold'>{name}</p>
--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -1,14 +1,21 @@
 import { createContext,  useEffect,  useState  } from "react";
-import { vehicle_list } from "../assets/assets";
-// import axios from 'axios';
+// import { vehicle_list } from "../assets/assets";
+import axios from 'axios';
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
     const url ="http://localhost:3005"
     const [token,setToken] = useState("");
+    const [vehicle_list,setVehicleList] = useState([]);
+
+    const fetchVehicleList = async () =>{
+        const response = await axios.get(url+"/api/car/list");
+        setVehicleList(response.data.data)
+    }
 
     useEffect(()=>{
         async function loadData(){
+            await fetchVehicleList();
             if(localStorage.getItem("token")){
                 setToken(localStorage.getItem("token")); 
             }
