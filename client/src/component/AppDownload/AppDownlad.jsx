362a3475e754d66480b3fa171d97f497b1d7e19d
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-07-07 13:02:56 +0530

    appdownload updated

--- client/src/component/AppDownload/AppDownlad.jsx
+++ client/src/component/AppDownload/AppDownlad.jsx
@@ -4,9 +4,10 @@ import app_store from '../../assets/app-store.png'
 
 const AppDownload = () => {
   return (
-    <div className='m-auto mt-6 flex flex-col justify-center items-center font-bold text:xl sm:text-2xl md:text-4xl' id='app-download'>
+    <div className='m-auto mt-6 flex flex-col justify-center items-center font-bold text:xl sm:text-2xl md:text-4xl py-10' id='app-download'>
         <p>For Better Experience Download </p>
         <p>Voyager App</p>
+        <p className='text-lg'>Download our app using Playstore or Appstore</p>
         <div className="flex flex-col justify-center mt-10 sm:flex-row">
             <img className='w-48 h-16 cursor-pointer' src={play_store} alt="" />&nbsp;
             <img className='w-48 h-16 cursor-pointer' src={app_store} alt="" />
