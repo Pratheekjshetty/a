b18b1e750b690483e1195c0e316c29e4dd45802c
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-07-07 13:57:28 +0530

    header updated

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 import front from '../../assets/front.jpg';
+
 const Header = () => {
   return (
-    <div>
-      <img className="" src={front} alt="Front" style={{ height: '700px', width: '100%' }} />
+    <div className="relative">
+      <div className="absolute top-0 left-0 ml-32 mt-64 text-white w-1/2">
+        <h1 className="font-medium text-white text-6xl  lg:text-7xl xl:text-8xl">
+          Rent a Best Car
+        </h1>
+        <p className="mt-4 text-sm hidden lg:block">
+        Our app provides a wide selection of vehicles to choose from, ensuring you find the perfect car for your journey. Whether you're planning a quick trip across town or a long-distance adventure, we have the right car for you. Our easy-to-use platform allows you to compare different models, book in advance, and enjoy a hassle-free rental experience. Join our community of satisfied customers and start your journey with us today!
+        </p>
+        <center><button className='bg-white text-indigo-600 p-2 rounded-lg text-md w-44 mt-8'>View Cars</button></center>
+      </div>
+      <img
+        className="w-full object-cover"
+        src={front}
+        alt="Front"
+        style={{ height: '700px' }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
